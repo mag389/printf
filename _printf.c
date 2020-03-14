91877b352d26cfa2b7988c865a7a0c69b435f31e
Michael George <mag389@cornell.edu>
2020-03-14 23:37:43 +0000

    file not printing correctly but gets and saves arguments mostly as it should, in process of testing locally, no sucess yet

--- _printf.c
+++ _printf.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "holberton.h"
 
 /**
@@ -11,33 +12,45 @@ int _printf(const char *format, ...)
 	va_list args, args_cpy;
 	char *buffer, *buffer_begin, *argtext, *flags, *chara;
 	char type, len;
-	int finlen, width;/*precision*/
+	int finlen, width, index = 0;/*precision*/
 
 	fmt_cpy = _cstrdup(format);
+	printf("%s\n",fmt_cpy);
 	buffer = malloc(500);
 	chara = _calloc(2,1,'\0');
 	if (buffer == NULL || chara == NULL)
 		exit(1);
-	buffer_begin = buffer;
+	buffer_begin = &(buffer[0]);
 	va_start(args, format);
 	va_copy(args_cpy,args);
+	printf("----------------------------------------\n");
 	while (*fmt_cpy)
 	{
+/*		printf("makes it into the while loop\n");*/
 		if (*fmt_cpy == '%')
 		{
 			if (*(fmt_cpy + 1) == '%')
 			{
+				printf("the char is: %c\n", *fmt_cpy);
 				save_to_buffer(&buffer, "%");
+				printf("trying a print iside the if %s\n", buffer);
 				fmt_cpy += 2;
+				index += 1;
 			}
 			else
 			{
+				printf("the char is: %c\n", *fmt_cpy);
 				flags = get_flags(&fmt_cpy);
 				get_width(&fmt_cpy);
-/*				get_precision*/
-				len = get_len(&fmt_cpy);
+/*				get_precision
+				len = get_len(&fmt_cpy);*/
 				type = get_type(&fmt_cpy);
-				argtext = _itoa(va_arg(args, long int), len);
+				(void)width;
+				(void)flags;
+				(void)type;
+				(void)len;
+				argtext = _itoa(va_arg(args, long int), 'l');
+				printf("the argtext is: %s\n",argtext);
 /*				if type is string call string f'n pointer
 				if type is number call number f'n pointer
 				apply_width() and precision
@@ -45,6 +58,8 @@ int _printf(const char *format, ...)
 	
 */				save_to_buffer(&buffer, argtext);
 				fmt_cpy++;
+				printf("the arglength in: %i\n",_strlen(argtext));
+				index += _strlen(argtext);
 			}
 		}
 		else
@@ -52,11 +67,17 @@ int _printf(const char *format, ...)
 			chara[0] = *fmt_cpy;
 			save_to_buffer(&buffer, chara);
 			fmt_cpy++;
+			index += 1;
 		}
 	}
+	printf("index is: %i\n", index);
 	*buffer = 0;
 	finlen = _strlen(buffer_begin);
-	printf("%s",buffer_begin);
+	printf("length is %i\n",finlen);
+	write(1, &buffer_begin - index, index);
+	printf("i just tried to write\nbuffer begin - index is %s\n",buffer_begin - index);
+	printf("printing just buffer: %s\n", (buffer - index + 1));
+	printf("and buffer begin: %s\n",buffer_begin - 2);
 	return(finlen);	
 /*	write buffer to output
 	free(everything allocated)
