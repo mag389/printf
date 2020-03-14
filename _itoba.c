7562b33d056920fd5994a0e4ceff6c77f59f1a4f
Michael George <mag389@cornell.edu>
2020-03-14 23:39:26 +0000

    converts unsigned long to int

---
+++ _itoba.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "holberton.h"
+long int mypow(int a, int b);
+
+/**
+*_itoba - takes an integer and returns it as a string
+*Return: char pointer containing the sring number
+*@number:the number taken in
+*@length: whether the number is long or short int
+*/
+
+void *_itoba(long int number, char length)
+{
+	int i = 0, digits, sign = 1, numcpy = number, signbuffer = 0;
+	char *array, *newarray;
+
+	(void) length;
+	array =  _calloc(50, 1, 'n');
+	if (array == NULL)
+		return (NULL);
+/*	printf("past calloc");*/
+/*	if (number < 0)
+	{
+		array[0] = '-';
+		sign = -1;
+		signbuffer++;
+	}*/
+/*	printf("past neg check\n");*/
+	for (digits = 1; digits < 50; digits++)
+	{
+		numcpy = numcpy / 2;
+		if (numcpy == 0)
+			break;
+	}
+/*	printf("got digits: %i\n",digits);*/
+	for (digits--; digits >= 0; digits--)
+	{
+		array[digits + signbuffer] = '0' + number % 2 * sign;
+		number /= 2;
+		i++;
+/*		printf("i:%i 1--array to now %s \n", i, array);*/
+	}
+	array[i + signbuffer] = '\0';
+	i += signbuffer;
+/*	printf("%s \n",array);*/
+	newarray = _calloc(i + 1 + signbuffer, 1, '\0');
+	for (; i >= 0; i--)
+		newarray[i] = array[i];
+	free(array);
+	return ((char *)newarray);
+}
