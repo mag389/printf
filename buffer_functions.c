0edbeb55f2515021d8cc888b964df9bbc4c21161
95ktsmith <95ktsmith@gmail.com>
2020-03-16 11:38:00 +0000

    Added comment blocks for each function and made Betty-compliant.

--- buffer_functions.c
+++ buffer_functions.c
@@ -1,5 +1,16 @@
 #include "holberton.h"
 
+/**
+ * extend_buffer - extend buffer
+ * Description: Extends the allocated space of the buffer by reallocating
+ *              a new space equal to the current size of the buffer and
+ *              the amount to extend it by, and copies the contents of the
+ *              buffer into the new space.
+ *
+ * @buffer: Pointer to buffer to extend.
+ * @extension: Number of bytes by whuch to extend the buffer.
+ * Return: Pointer to new buffer space.
+ */
 char *extend_buffer(char *buffer, int extension)
 {
 	int buffer_size, new_size;
@@ -8,18 +19,33 @@ char *extend_buffer(char *buffer, int extension)
 	buffer_size = _strlen(buffer) + 1;
 	new_size = buffer_size + extension;
 	new_buffer = _realloc(buffer, buffer_size, new_size);
-	return(new_buffer);
+	return (new_buffer);
 }
 
+/**
+ * init_buffer - initialize buffer
+ * Description: Callocs a single null byte to start the buffer with. Mainly
+ *              just to save lines in _printf.
+ * Return: Pointer to allocated buffer.
+ */
 char *init_buffer(void)
 {
 	char *buffer;
+
 	buffer = _calloc(1, 1, '\0');
-	if(!buffer)
-		return (NULL);
+	if (!buffer)
+		exit(1000);
 	return (buffer);
 }
 
+/**
+ * save_to_buffer - save to buffer
+ * Desciption: Appends a string to the end of the local buffer.
+ * @buffer: Double pointer to local buffer. Needed as a double to extend with
+ *          extend_buffer function.
+ * @f_text: String of text to save to the buffer.
+ * Return: The length of the string appended to the buffer.
+ */
 int save_to_buffer(char **buffer, char *f_text)
 {
 	int f_text_len, index, old_buffer_len;
@@ -28,11 +54,12 @@ int save_to_buffer(char **buffer, char *f_text)
 	f_text_len = _strlen(f_text);
 
 	*buffer = extend_buffer(*buffer, f_text_len);
-	if(!buffer)
+	if (!buffer)
 		return (0);
 	for (index = 0; *(f_text + index); index++)
 		*(*(buffer) + old_buffer_len + index) = *(f_text + index);
 	*(*(buffer) + old_buffer_len + index) = '\0';
-	/* free(f_text); */
-	return(f_text_len);
+
+	free(f_text);
+	return (f_text_len);
 }
